7f4e5dd3b62693c1b2093c9edae18cbb20e2be7a
Trippasch <pasxoshorp13@gmail.com>
2022-12-08 15:01:56 +0200

    Mesh textures clean up

--- OpenGL/src/Renderer/Mesh.cpp
+++ OpenGL/src/Renderer/Mesh.cpp
@@ -51,31 +51,8 @@ void Mesh::UnBind() const
 // render the mesh
 void Mesh::Draw(const Shader &shader) const
 {
-    // bind appropriate textures
-    unsigned int diffuseNr = 1;
-    unsigned int specularNr = 1;
-    unsigned int normalNr = 1;
-    unsigned int heightNr = 1;
     for (unsigned int i = 0; i < textures.size(); i++)
-    {
-        glActiveTexture(GL_TEXTURE0 + i); // active proper texture unit before binding
-        // retrieve texture number (the N in diffuse_textureN)
-        std::string number;
-        std::string name = textures[i].Type;
-        if (name == "texture_diffuse")
-            number = std::to_string(diffuseNr++);
-        else if (name == "texture_specular")
-            number = std::to_string(specularNr++); // transfer unsigned int to string
-        else if (name == "texture_normal")
-            number = std::to_string(normalNr++); // transfer unsigned int to string
-        else if (name == "texture_height")
-            number = std::to_string(heightNr++); // transfer unsigned int to string
-
-        // now set the sampler to the correct texture unit
-        glUniform1i(glGetUniformLocation(shader.ID, (name + number).c_str()), i);
-        // and finally bind the texture
-        glBindTexture(GL_TEXTURE_2D, textures[i].ID);
-    }
+        textures[i].Bind(i);
 
     // draw mesh
     Bind();
